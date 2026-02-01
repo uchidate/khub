b8107fb1f251a1baae6c6aa2b995f32b50571d93
Fabio Uchidate <fabiouchidate@gmail.com>
2026-02-01 19:18:02 -0300

    fix: use standard prisma client import

--- v1/scripts/full-seed.js
+++ v1/scripts/full-seed.js
@@ -1,4 +1,4 @@
-const { PrismaClient } = require("./node_modules/.prisma/client");
+const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 async function main() {
